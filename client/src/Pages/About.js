f2ae752a5da1475a382165aec15e87c4fd54df27
HENOCK A.SOLOMON <117090208+HenockSolomon@users.noreply.github.com>
2023-04-18 15:34:32 +0100

    last day

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -4,7 +4,13 @@ function About() {
   return (
     <div>
       <Navbar></Navbar>
-      <div>This page is about us</div>
+      <div>
+        <h1>This page is about us
+        </h1>
+        <h2>
+          This website is made by <a href="http://github.com/Group-6-Alpha-Henock/Alpha-Henock">Henock</a> and <a href="https://github.com/Group-6-Alpha-Henock/Alpha-Henock">Alpha</a> in collaboration.
+        </h2>
+      </div>
     </div>
   );
 }
